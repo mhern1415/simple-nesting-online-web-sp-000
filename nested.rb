e186895324ca80a15ebc76210fb384cbf2657f35
Mark Hernandez <m.hern1415@gmail.com>
2019-05-10 19:39:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,8 +62,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-:yukihiro_matsumoto => { :known_for => "Ruby", :languages => ["LISP", "C"]}
-
+# :yukihiro_matsumoto => { :known_for => "Ruby", :languages => ["LISP", "C"]}
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,7 +78,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = 
     
 end
 
