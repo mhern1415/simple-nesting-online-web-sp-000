36e408307c221d1c8c58e0a3f9b55ccbf8e1070e
Mark Hernandez <m.hern1415@gmail.com>
2019-05-10 19:52:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -121,5 +121,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie]
+
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
